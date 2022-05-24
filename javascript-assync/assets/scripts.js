0f0e81cb59fa8c214fbb210b821d848c3f77483a
Paulo Henrique <paulo-henrique-morada@hotmail.com>
2022-05-24 18:57:37 -0300

    Aula pratica DOM

--- javascript-assync/assets/scripts.js
+++ javascript-assync/assets/scripts.js
@@ -1,21 +1,19 @@
 const BASE_URL = 'https://thatcopy.pw/catapi/rest/';
 const catBtn = document.getElementById('change-cat');
-const catIMg = document.getElementById('cat');
 
 const getCats = async () => {
-    try {
-        const data = await fetch(BASE_URL);
-        const json = await data.json();
 
-        return json.webpurl;
-    } catch (error) {
-        console.log(error.message);
-    }
+        const data = await fetch(BASE_URL)
+                    .then((res) => res.json())
+                    .catch((e) => console.log(e));
+
+        return data.webpurl;
     
 }
 
 const loadImg = async () => {
-    catIMg.src = await getCats();
+    const catImg = document.getElementById('cat');
+    catImg.src = await getCats();
 }
 
 catBtn.addEventListener('click', loadImg);
